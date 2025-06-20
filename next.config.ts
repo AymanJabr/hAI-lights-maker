318554ffe58249dc9f297b426cd31cb17aab90f8
AymanJabr <aymanjaber2012@gmail.com>
2025-06-20 14:05:22 +0300

    chore trying to completly remove the Cross-Origin-Embedder-Policy from all files

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  headers: async () => {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "Cross-Origin-Embedder-Policy",
-            value: "require-corp",
-          },
-          {
-            key: "Cross-Origin-Opener-Policy",
-            value: "same-origin",
-          },
-        ],
-      },
-    ];
-  },
+  // headers: async () => {
+  //   return [
+  //     {
+  //       source: "/:path*",
+  //       headers: [
+  //         {
+  //           key: "Cross-Origin-Embedder-Policy",
+  //           value: "require-corp",
+  //         },
+  //         {
+  //           key: "Cross-Origin-Opener-Policy",
+  //           value: "same-origin",
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
   webpack: (config) => {
     // Support for WebAssembly
     config.experiments = { ...config.experiments, asyncWebAssembly: true };
